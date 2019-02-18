266893bb603a5352bcbdd3c604f8e7dd02477a86
root <husammustafa35@gmail.com>
2019-02-18 12:33:18 -0800

    12.0

---
+++ leaves_limit/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Leaves Request Limit",
+
+    'summary': """
+        Add Features To Limit Leave Request For Specific Leave Type By Month Or Year """,
+
+    'description': """
+        This Module Will Add Features To Limit Leave Request For Specific Leave Type By Month Or Year
+        Or You Can Choose Both , And Limit Amount Will Be Changed As Needed For Each Leave Type
+    """,
+
+    'author': "Smart Solutions",
+    'website': "",
+    'category': 'Human Resources',
+    'version': '1.0',
+    'depends': ['hr_holidays'],
+    'data': [
+        'views/views.xml',
+    ],
+    'demo': [
+    ],
+    'images': ['static/description/img-1.PNG',
+               'static/description/img-2.PNG'],
+    'live_test_url': 'https://www.youtube.com/watch?v=8ib59aV1Tfs',
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+    'price': 10,
+    'currency': 'EUR',
+    'support': 'smartsolutionsit99@gmail.com',
+}
